d71263bf03a4e1146131c3aebc7efb32a92db56b
kirstinmeyer <kmeyer@whoi.edu>
2021-03-23 12:54:18 -0400

    Add files via upload

---
+++ Image-capture-software/packages/larvaecam-packages-u1910-arm64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install net-tools
+sudo apt install openssh-server
+sudo apt install libusb-1.0-0
+sudo apt install libusb-1.0-0-dev
+sudo apt install git
+# The next line is only if you want a desktop environment
+#sudo apt install xubuntu-desktop^ slick-greeter
+sudo apt install python-pip
+sudo apt install python3-pip
+sudo apt install pkg-config
+sudo apt install libfreetype6-dev
+sudo apt install libjpeg-dev
+sudo apt install emacs
+sudo apt install unzip
+sudo apt install cmatrix
+sudo apt install timedatectl
+sudo apt install gtkterm
